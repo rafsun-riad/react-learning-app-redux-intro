b423d27daa2772704dedeb1ae92f71ba73167c39
rafsun-riad <mruhaquer@gmail.com>
2023-12-12 20:16:59 +0600

    learned about redux thunk

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -1,78 +1,109 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deposit, payLoan, requestLoan, withdraw } from './accountSlice';
 
 function AccountOperations() {
-  const [depositAmount, setDepositAmount] = useState("");
-  const [withdrawalAmount, setWithdrawalAmount] = useState("");
-  const [loanAmount, setLoanAmount] = useState("");
-  const [loanPurpose, setLoanPurpose] = useState("");
-  const [currency, setCurrency] = useState("USD");
+	const [depositAmount, setDepositAmount] = useState('');
+	const [withdrawalAmount, setWithdrawalAmount] = useState('');
+	const [loanAmount, setLoanAmount] = useState('');
+	const [loanPurpose, setLoanPurpose] = useState('');
+	const [currency, setCurrency] = useState('USD');
 
-  function handleDeposit() {}
+	const dispatch = useDispatch();
+	const {
+		loan: currentLoan,
+		loanPurpose: currentLoanPurpose,
+		isLoading,
+	} = useSelector((store) => store.account);
 
-  function handleWithdrawal() {}
+	function handleDeposit() {
+		if (!depositAmount) return;
+		dispatch(deposit(depositAmount, currency));
+		setDepositAmount('');
+		setCurrency('');
+	}
 
-  function handleRequestLoan() {}
+	function handleWithdrawal() {
+		if (!withdrawalAmount) return;
+		dispatch(withdraw(withdrawalAmount));
+		setWithdrawalAmount('');
+	}
 
-  function handlePayLoan() {}
+	function handleRequestLoan() {
+		if (!loanAmount || !loanPurpose) return;
+		dispatch(requestLoan(loanAmount, loanPurpose));
+		setLoanAmount('');
+		setLoanPurpose('');
+	}
 
-  return (
-    <div>
-      <h2>Your account operations</h2>
-      <div className="inputs">
-        <div>
-          <label>Deposit</label>
-          <input
-            type="number"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(+e.target.value)}
-          />
-          <select
-            value={currency}
-            onChange={(e) => setCurrency(e.target.value)}
-          >
-            <option value="USD">US Dollar</option>
-            <option value="EUR">Euro</option>
-            <option value="GBP">British Pound</option>
-          </select>
+	function handlePayLoan() {
+		dispatch(payLoan());
+	}
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
-        </div>
+	return (
+		<div>
+			<h2>Your account operations</h2>
+			<div className="inputs">
+				<div>
+					<label>Deposit</label>
+					<input
+						type="number"
+						value={depositAmount}
+						onChange={(e) => setDepositAmount(+e.target.value)}
+					/>
+					<select
+						value={currency}
+						onChange={(e) => setCurrency(e.target.value)}
+					>
+						<option value="USD">US Dollar</option>
+						<option value="EUR">Euro</option>
+						<option value="GBP">British Pound</option>
+					</select>
 
-        <div>
-          <label>Withdraw</label>
-          <input
-            type="number"
-            value={withdrawalAmount}
-            onChange={(e) => setWithdrawalAmount(+e.target.value)}
-          />
-          <button onClick={handleWithdrawal}>
-            Withdraw {withdrawalAmount}
-          </button>
-        </div>
+					<button onClick={handleDeposit} disabled={isLoading}>
+						{isLoading
+							? 'Converting...'
+							: `Deposit ${depositAmount}`}
+					</button>
+				</div>
 
-        <div>
-          <label>Request loan</label>
-          <input
-            type="number"
-            value={loanAmount}
-            onChange={(e) => setLoanAmount(+e.target.value)}
-            placeholder="Loan amount"
-          />
-          <input
-            value={loanPurpose}
-            onChange={(e) => setLoanPurpose(e.target.value)}
-            placeholder="Loan purpose"
-          />
-          <button onClick={handleRequestLoan}>Request loan</button>
-        </div>
+				<div>
+					<label>Withdraw</label>
+					<input
+						type="number"
+						value={withdrawalAmount}
+						onChange={(e) => setWithdrawalAmount(+e.target.value)}
+					/>
+					<button onClick={handleWithdrawal}>
+						Withdraw {withdrawalAmount}
+					</button>
+				</div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
-      </div>
-    </div>
-  );
+				<div>
+					<label>Request loan</label>
+					<input
+						type="number"
+						value={loanAmount}
+						onChange={(e) => setLoanAmount(+e.target.value)}
+						placeholder="Loan amount"
+					/>
+					<input
+						value={loanPurpose}
+						onChange={(e) => setLoanPurpose(e.target.value)}
+						placeholder="Loan purpose"
+					/>
+					<button onClick={handleRequestLoan}>Request loan</button>
+				</div>
+
+				<div>
+					<span>
+						Pay back ${currentLoan} {currentLoanPurpose}
+					</span>
+					<button onClick={handlePayLoan}>Pay loan</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AccountOperations;
--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,6 +2,7 @@ const initialStateAccount = {
 	balance: 0,
 	loan: 0,
 	loanPurpose: '',
+	isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
@@ -10,6 +11,7 @@ export default function accountReducer(state = initialStateAccount, action) {
 			return {
 				...state,
 				balance: state.balance + action.payload,
+				isLoading: false,
 			};
 
 		case 'account/withdraw':
@@ -27,7 +29,7 @@ export default function accountReducer(state = initialStateAccount, action) {
 				balance: state.balance + action.payload.amount,
 			};
 
-		case 'action/payLoan':
+		case 'account/payLoan':
 			return {
 				...state,
 				loan: 0,
@@ -35,13 +37,26 @@ export default function accountReducer(state = initialStateAccount, action) {
 				balance: state.balance - state.loan,
 			};
 
+		case 'account/convertingCurrency':
+			return { ...state, isLoading: true };
+
 		default:
 			return state;
 	}
 }
 
-export function deposit(amount) {
-	return { type: 'account/deposit', payload: amount };
+export function deposit(amount, currency) {
+	if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+	return async function (dispatch, getState) {
+		dispatch({ type: 'account/convertingCurrency' });
+		const res = await fetch(
+			`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+		);
+		const data = await res.json();
+		const converted = data.rates.USD;
+
+		dispatch({ type: 'account/deposit', payload: converted });
+	};
 }
 
 export function withdraw(amount) {
